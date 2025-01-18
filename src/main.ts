370b56cc3f92fe836d1f5203c91d0c54aa639acd
Juan Pablo Buelvas <42744380+jpbuelvas@users.noreply.github.com>
2025-01-18 01:47:07 -0500

    Fixing deployment errors

--- src/main.ts
+++ src/main.ts
@@ -4,12 +4,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
    // Configurar CORS
-   app.enableCors({
-    origin: 'http://localhost:3001', // Permitir este origen
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE', // Métodos HTTP permitidos
-    credentials: true, // Si envías cookies o autorizaciones
-  });
-
+   app.enableCors()
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
